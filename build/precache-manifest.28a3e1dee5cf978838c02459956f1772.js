82081314f8791d12adda7fc642f5217bb66d3510
El hadji.M.sabaly <hadjisabs@gmail.com>
2020-08-05 21:25:20 +0200

    Add files via upload

---
+++ build/precache-manifest.28a3e1dee5cf978838c02459956f1772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07aa6f1350f6fc05c994e873c60aab59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "399825bf776b6401d0d1",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8c39b17150a3cd38a4ed",
+    "url": "/static/js/2.a9ec043e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a9ec043e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "399825bf776b6401d0d1",
+    "url": "/static/js/main.75b06344.chunk.js"
+  },
+  {
+    "revision": "d43b119cb80e67dd34d5",
+    "url": "/static/js/runtime-main.a4949ede.js"
+  }
+]);
